5b851737f18fa24a2ced5bc5976cb60e95c6cf18
Кобозев Александр <a.kobozev@corp.mail.ru>
2020-02-08 12:22:51 +0300

    update versions, remove rendurants from player

--- app/src/main/java/com/wishnewjam/dubstepfm/MediaPlayerInstance.kt
+++ app/src/main/java/com/wishnewjam/dubstepfm/MediaPlayerInstance.kt
@@ -1,77 +1,62 @@
 package com.wishnewjam.dubstepfm
 
 import android.content.Context
-import android.media.AudioAttributes
-import android.media.AudioFocusRequest
-import android.media.AudioManager
-import android.net.wifi.WifiManager
-import android.os.Build
-import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlaybackException
-import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.PlaybackParameters
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.Timeline
-import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.metadata.icy.IcyInfo
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
-import okhttp3.OkHttpClient
-import saschpe.exoplayer2.ext.icy.IcyHttpDataSourceFactory
 
 class MediaPlayerInstance(private val context: Context) : Player.EventListener {
     companion object {
         private const val USER_AGENT: String = "dubstep.fm"
-        private const val WAKE_LOCK = "mp_wakelock"
     }
 
     var status: Int = UIStates.STATUS_UNDEFINED
     var serviceCallback: CallbackInterface? = null
     private var currentUrl: CurrentUrl = CurrentUrl(context)
-
-    private var audioManager: AudioManager? =
-            ContextCompat.getSystemService(context, AudioManager::class.java)
-
-    private var focusRequest: AudioFocusRequest? = null
-    private var wifiLock: WifiManager.WifiLock?
-    private var mediaPlayer: SimpleExoPlayer?
+    private val mediaPlayer: SimpleExoPlayer = SimpleExoPlayer.Builder(context)
+            .build()
 
     init {
-
-        val trackSelector = DefaultTrackSelector(
-                AdaptiveTrackSelection.Factory(DefaultBandwidthMeter()))
-
-        mediaPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector)
-        mediaPlayer?.addListener(this)
-
-        wifiLock = (context.applicationContext.getSystemService(
-                Context.WIFI_SERVICE) as WifiManager).createWifiLock(
-                WifiManager.WIFI_MODE_FULL, WAKE_LOCK)
-
+        mediaPlayer.addListener(this)
+        val attributes = AudioAttributes.Builder()
+                .setUsage(C.USAGE_MEDIA)
+                .setContentType(C.CONTENT_TYPE_MUSIC)
+                .build()
+        mediaPlayer.audioAttributes = attributes
+        mediaPlayer.addMetadataOutput {
+            if (it.length() > 0) {
+                (it.get(0) as? IcyInfo?)?.title?.let { s ->
+                    serviceCallback?.onMetaDataTrackChange(s)
+                }
+            }
+        }
     }
 
     override fun onRepeatModeChanged(repeatMode: Int) {
         Tools.logDebug { "exoPlayer: onRepeatModeChanged: $repeatMode" }
     }
 
-    override fun onPlaybackParametersChanged(
-            playbackParameters: PlaybackParameters?) {
+    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
         Tools.logDebug { "exoPlayer: onPlaybackParametersChanged: $playbackParameters" }
     }
 
-    override fun onTracksChanged(trackGroups: TrackGroupArray?,
-                                 trackSelections: TrackSelectionArray?) {
+    override fun onTracksChanged(trackGroups: TrackGroupArray,
+                                 trackSelections: TrackSelectionArray) {
         Tools.logDebug { "exoPlayer: onTracksChanged: trackGroups = $trackGroups, trackSelections = $trackSelections" }
 
     }
 
-    override fun onPlayerError(error: ExoPlaybackException?) {
+    override fun onPlayerError(error: ExoPlaybackException) {
         Tools.logDebug { "exoPlayer: onPlayerError: error = $error" }
         status = UIStates.STATUS_ERROR
         serviceCallback?.onError("Playback error: $error")
@@ -86,6 +71,7 @@ class MediaPlayerInstance(private val context: Context) : Player.EventListener {
             Player.STATE_READY     -> if (playWhenReady) notifyStatusChanged(
                     UIStates.STATUS_PLAY)
             Player.STATE_ENDED     -> notifyStatusChanged(UIStates.STATUS_STOP)
+            Player.STATE_IDLE      -> return
         }
         Tools.logDebug { "exoPlayer: onPlayerStateChanged: playWhenReady = $playWhenReady, playbackState = $playbackState " }
     }
@@ -98,7 +84,7 @@ class MediaPlayerInstance(private val context: Context) : Player.EventListener {
         Tools.logDebug { "exoPlayer: onPositionDiscontinuity" }
     }
 
-    override fun onTimelineChanged(timeline: Timeline?, manifest: Any?,
+    override fun onTimelineChanged(timeline: Timeline, manifest: Any?,
                                    reason: Int) {
         Tools.logDebug { "exoPlayer: onTimelineChanged: timeline = $timeline, manifest = $manifest" }
     }
@@ -132,16 +118,8 @@ class MediaPlayerInstance(private val context: Context) : Player.EventListener {
     fun callStop() {
         when (status) {
             UIStates.STATUS_PLAY -> {
-                mediaPlayer?.stop()
+                mediaPlayer.stop()
                 notifyStatusChanged(UIStates.STATUS_STOP)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    focusRequest?.let {
-                        audioManager?.abandonAudioFocusRequest(it)
-                    }
-                }
-                else {
-                    audioManager?.abandonAudioFocus(afChangeListener)
-                }
             }
         }
     }
@@ -153,19 +131,6 @@ class MediaPlayerInstance(private val context: Context) : Player.EventListener {
         }
     }
 
-    private val afChangeListener: AudioManager.OnAudioFocusChangeListener
-        get() = AudioManager.OnAudioFocusChangeListener { focusChange ->
-            when (focusChange) {
-                AudioManager.AUDIOFOCUS_LOSS, AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> if (mediaPlayer?.playbackState == Player.STATE_READY) {
-                    mediaPlayer?.stop()
-                    notifyStatusChanged(UIStates.STATUS_STOP)
-                }
-                AudioManager.AUDIOFOCUS_GAIN                                         -> if (status == UIStates.STATUS_WAITING) {
-                    callPlay()
-                }
-            }
-        }
-
     private fun notifyStatusChanged(status: Int) {
         this.status = status
         serviceCallback?.onChangeStatus(status)
@@ -173,45 +138,13 @@ class MediaPlayerInstance(private val context: Context) : Player.EventListener {
 
     private fun play() {
         val source = currentUrl.currentUrl.toUri()
-
-        val client = OkHttpClient.Builder()
-                .build()
-        val icyHttpDataSourceFactory = IcyHttpDataSourceFactory.Builder(client)
-                .setUserAgent(Util.getUserAgent(context, USER_AGENT))
-                .setIcyMetadataChangeListener { icyMetadata ->
-                    serviceCallback?.onMetaDataTrackChange(
-                            icyMetadata.streamTitle)
-                }
-                .build()
-
-        val dataSourceFactory = DefaultDataSourceFactory(context, null,
-                icyHttpDataSourceFactory)
-
-        val mediaSourceFactory = ExtractorMediaSource.Factory(dataSourceFactory)
-        val mediaSource = mediaSourceFactory.createMediaSource(source)
-        mediaPlayer?.playWhenReady = true
-        mediaPlayer?.prepare(mediaSource)
+        val dataSourceFactory = DefaultDataSourceFactory(context, USER_AGENT)
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(source)
+        mediaPlayer.playWhenReady = true
+        mediaPlayer.prepare(mediaSource)
 
         notifyStatusChanged(UIStates.STATUS_LOADING)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val playbackAttributes = AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .build()
-            focusRequest =
-                    AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                            .setAudioAttributes(playbackAttributes)
-                            .setAcceptsDelayedFocusGain(true)
-                            .setOnAudioFocusChangeListener { afChangeListener }
-                            .build()
-            focusRequest?.let { audioManager?.requestAudioFocus(it) }
-        }
-        else {
-            audioManager?.requestAudioFocus(afChangeListener,
-                    AudioManager.STREAM_MUSIC,
-                    AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
-        }
     }
 
     interface CallbackInterface {
--- app/src/main/java/com/wishnewjam/dubstepfm/MyApplication.kt
+++ app/src/main/java/com/wishnewjam/dubstepfm/MyApplication.kt
@@ -1,20 +1,9 @@
 package com.wishnewjam.dubstepfm
 
 import android.app.Application
-import com.squareup.leakcanary.LeakCanary
 
 class MyApplication : Application() {
     val mediaPlayerInstance: MediaPlayerInstance by lazy {
         MediaPlayerInstance(this)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-        LeakCanary.install(this)
     }
 }
