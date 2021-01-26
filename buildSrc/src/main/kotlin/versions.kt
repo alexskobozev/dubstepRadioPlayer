78aef35305317691b26b2c296adcd26e76ab8b20
Кобозев Александр <a.kobozev@corp.mail.ru>
2021-01-26 12:39:59 +0300

    update versions

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,8 +1,8 @@
-const val KOTLIN_VERSION = "1.4.10"
+const val KOTLIN_VERSION = "1.4.21-2"
 
 object Plugins {
     const val AGP = "4.1.2"
-    const val VERSIONS = "0.28.0"
+    const val VERSIONS = "0.36.0"
 
     // don't update https://stackoverflow.com/questions/54498721/why-im-getting-java-lang-arrayindexoutofboundsexception-when-i-add-the-plugin-co
     // i've updated, if troubles, downgrade to 4.1
@@ -13,18 +13,18 @@ object Plugins {
 object AndroidX {
     const val MULTIDEX = "2.0.1"
     const val APPCOMPAT = "1.2.0"
-    const val MEDIA = "1.2.0"
-    const val CONSTRAINT = "2.0.2"
+    const val MEDIA = "1.2.1"
+    const val CONSTRAINT = "2.0.4"
     const val LIFECYCLE = "2.2.0"
 }
 
 object Libs {
-    const val EXOPLAYER = "2.12.0"
+    const val EXOPLAYER = "2.12.3"
     const val LEAK = "2.5"
     const val WANG = "2.1.3"
     const val KTX = "1.3.2"
-    const val FIREBASE = "17.5.1"
-    const val FIREBASE_CRASH = "17.2.2"
+    const val FIREBASE = "18.0.1"
+    const val FIREBASE_CRASH = "17.3.0"
     const val MATERIAL_DIALOGS = "3.3.0"
 }
 
