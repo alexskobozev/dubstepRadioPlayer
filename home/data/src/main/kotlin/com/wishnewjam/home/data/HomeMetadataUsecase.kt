419a125a63ddac4ccff6dcceef31e97bcaf4de67
Alexander Kobozev <alexskobozev@gmail.com>
2024-06-21 14:49:17 +0400

    update metadata usecase

--- home/data/src/main/kotlin/com/wishnewjam/home/data/HomeMetadataUsecase.kt
+++ home/data/src/main/kotlin/com/wishnewjam/home/data/HomeMetadataUsecase.kt
@@ -3,11 +3,12 @@ package com.wishnewjam.home.data
 import com.wishnewjam.home.domain.MetadataUsecase
 import com.wishnewjam.metadata.domain.MetadataRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class HomeMetadataUsecase @Inject constructor(
-    val repository: MetadataRepository,
+    private val repository: MetadataRepository,
 ) : MetadataUsecase {
     override val playingText: Flow<String>
-        get() = TODO("Not yet implemented")
+        get() = repository.currentTrack.map { it.title }
 }
