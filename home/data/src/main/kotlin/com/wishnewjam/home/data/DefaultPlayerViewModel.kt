f44e8f3acb841e8485dec5719b6264d40c56471b
Alexander Kobozev <alexskobozev@gmail.com>
2024-06-21 14:49:17 +0400

    update metadata usecase

--- home/data/src/main/kotlin/com/wishnewjam/home/data/DefaultPlayerViewModel.kt
+++ home/data/src/main/kotlin/com/wishnewjam/home/data/DefaultPlayerViewModel.kt
@@ -1,16 +1,19 @@
 package com.wishnewjam.home.data
 
+import androidx.lifecycle.viewModelScope
 import com.wishnewjam.home.domain.MetadataUsecase
 import com.wishnewjam.home.domain.PlayerViewModel
 import com.wishnewjam.playback.domain.PlaybackCommandHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 class DefaultPlayerViewModel @Inject constructor(
-    metadataUsecase: MetadataUsecase,
+    private val metadataUsecase: MetadataUsecase,
     private val playbackCommandHandler: PlaybackCommandHandler,
 ) : PlayerViewModel() {
 
@@ -21,10 +24,16 @@ class DefaultPlayerViewModel @Inject constructor(
     override fun play() {
         Timber.d("Push play button")
         playbackCommandHandler.play()
+        viewModelScope.launch {
+            metadataUsecase.playingText.collectLatest { playingText ->
+                _state.value = _state.value.copy(nowPlaying = playingText)
+            }
+        }
     }
 
     override fun stop() {
         Timber.d("Push stop button")
         playbackCommandHandler.stop()
+        _state.value = _state.value.copy(nowPlaying = "Stopped")
     }
 }
