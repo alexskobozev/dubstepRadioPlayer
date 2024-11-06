b75da1d04763217a3df24dcdbc2419786253cb99
Alexander Kobozev <alexskobozev@gmail.com>
2024-11-06 12:57:22 +0400

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.wishnewjam.dubstepfm"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10307
-        versionName = "1.3.7"
+        versionCode = 10308
+        versionName = "1.3.8"
     }
     buildTypes {
         getByName("release") {
